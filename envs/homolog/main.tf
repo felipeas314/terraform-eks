34c848150256f2b95e29a7934387f35947eee095
Felipe Alexandre <felipeas314@gmail.com>
2025-03-13 08:17:14 -0300

    refactor: mudança na criaçao do cluster

--- envs/homolog/main.tf
+++ envs/homolog/main.tf
@@ -59,3 +59,37 @@ module "eks" {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+  # Impede que o provider tente usar configurações locais do kubeconfig
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "kong" {
+  name             = "kong"
+  repository       = "https://charts.konghq.com"
+  chart            = "kong"
+  version          = "2.13.0" # Ajuste para a versão desejada
+  namespace        = "kong"
+  create_namespace = true
+
+  # Exemplo de customização via um arquivo de valores, se necessário.
+}
